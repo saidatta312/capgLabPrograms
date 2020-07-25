1740c8b60bc13cdd1cfc8ee97f5d1fc32188d373
Devuomsaiswaroop <Devuomsaiswaroop@DESKTOP-7BFNCK8>
2020-07-25 14:03:01 +0530

    hello

--- practiceTest/src/main/java/com/capg/practicetest/sortingstudents/SortingBasedOnN_A_H.java
+++ practiceTest/src/main/java/com/capg/practicetest/sortingstudents/SortingBasedOnN_A_H.java
@@ -2,6 +2,7 @@ package com.capg.practicetest.sortingstudents;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -21,7 +22,7 @@ public class SortingBasedOnN_A_H {
 			);
 	
 	
-	Set<Student> studentSet=new HashSet(students);
+	Set<Student> studentSet=new HashSet<>(students);
 	
 	
 	
@@ -32,11 +33,27 @@ public class SortingBasedOnN_A_H {
 	System.out.println();
 	System.out.println("Student List - ");
 	students.forEach(t->System.out.println(t));
-			
-	
-	
 	
 }
 
 }
+class SortByAge implements Comparator<Student>{
+@Override
+public int compare(Student s1, Student s2) {
+	return s1.age-s2.age;
+}
+}
 
+class SortByName implements Comparator<Student>{
+@Override
+public int compare(Student s1, Student s2) {
+	return s1.name.compareTo(s2.name);
+}
+}
+
+class SortByHeight implements Comparator<Student>{
+@Override
+public int compare(Student s1, Student s2) {
+	return s1.height.compareTo(s2.height);
+}
+}
