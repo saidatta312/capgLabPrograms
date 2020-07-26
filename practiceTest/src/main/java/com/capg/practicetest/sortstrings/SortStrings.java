c11583c6474cd2e0480d7c0c6d00546882d9b347
Devuomsaiswaroop <Devuomsaiswaroop@DESKTOP-7BFNCK8>
2020-07-26 20:18:02 +0530

    week 1 Q1 & Q2

--- practiceTest/src/main/java/com/capg/practicetest/sortstrings/SortStrings.java
+++ practiceTest/src/main/java/com/capg/practicetest/sortstrings/SortStrings.java
@@ -1,40 +1,50 @@
 package com.capg.practicetest.sortstrings;
 
+import java.util.Scanner;
+
 public class SortStrings {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String names[]= {"sai","om","swaroop","datta"};
-		int l=names.length;
-		sort(names,l); 
-	    
-	    printArraystring(names,l);
 		
+		Scanner scan=new Scanner(System.in);
+
+		 String names[]= new String[5];
+		for (int i = 0; i < names.length; i++) {
+			System.out.println("Enter the string");
+			 names[i]= scan.next();
+			
+		}
+		int l = names.length;
+		sort(names, l);
+
+		printArraystring(names, l);
 
 	}
-	static void sort(String []s, int l) 
-	{ 
-		String temp,sortedStr[];
-	    for(int i=0;i<l;i++) {
-	    	for(int j=0;j<i;j++) {
-				/*
-				 * if(s[i].length()>s[i+1].length()) { temp=s[i]; s[i]=s[i+1]; s[i+1]=temp;
-				 */
-	    		if(s[j].length()>s[j+1].length())
-	    		{ 
-	    			temp=s[j]; 
-	    			s[j]=s[j+1];
-	    			s[j+1]=temp;
-	    		}
-	    	}
-	    }
-	} 
-	  
-	
-	static void printArraystring(String str[], int l) 
-	{ 
-	    for (int i=0; i<l; i++) 
-	        System.out.print(str[i]+" "); 
-	} 
-	  
+
+	static void sort(String[] s, int l) {
+		String temp;
+		for (int i = 0; i < l; i++) {
+			for (int j = 0; j < l - 1; j++) {
+
+				if (s[j].length() > s[j + 1].length()) {
+					temp = s[j];
+					s[j] = s[j + 1];
+					s[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	static void printArraystring(String str[], int l) {
+		for (int i = 0; i < l; i++)
+			System.out.print(str[i] + " ");
+	}
+
 }
+
+/*
+ * for(int i=0; i < args.length; i++){ for(int j=1; j < (args.length-i); j++){
+ * if(args[j-1].length() > args[j].length()){
+ * 
+ * temp = args[j-1]; args[j-1] = args[j]; args[j] = temp; } }
+ */
